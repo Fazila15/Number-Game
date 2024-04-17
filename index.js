72a627c3fb5ba6c65375dbc0faed4d94672e640b
fazila15e <fazilamalik15@gmail.com>
2024-04-18 01:49:03 +0500

    Play again feature done!!

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
+#! /usr/bin/env node
 import inquirer from "inquirer";
 let randomNumber = Math.floor(Math.random() * 10 + 1);
-let userNumber = await inquirer.prompt({
-    name: "userGuessed",
-    message: "Guess a number: ",
-    type: "number",
-});
-if (userNumber.userGuessed === randomNumber) {
-    console.log("Congrats!!! You Guessed Right Number.");
-}
-else
-    ([
-        console.log(`Sorry Better Luck Next Time
+let condition = true;
+while (condition) {
+    let userNumber = await inquirer.prompt({
+        name: "userGuessed",
+        message: "Guess a number: ",
+        type: "number",
+    });
+    if (userNumber.userGuessed === randomNumber) {
+        console.log("Congrats!!! You Guessed Right Number.");
+    }
+    else
+        ([
+            console.log(`Sorry Better Luck Next Time
     The number was: ${randomNumber}`)
-    ]);
+        ]);
+    let usrAnswer = await inquirer.prompt({
+        name: "replay",
+        type: "confirm",
+        message: "Do you want to play again??",
+        default: "true",
+    });
+    condition = usrAnswer.replay;
+}
+;
--- index.ts
+++ index.ts
@@ -1,7 +1,11 @@
+#! /usr/bin/env node
 import inquirer from "inquirer";
 
+
 let randomNumber = Math.floor(Math.random()*10+1);
-let userNumber = await inquirer.prompt(
+let condition = true;
+while(condition){
+    let userNumber = await inquirer.prompt(
     {
         name: "userGuessed",
         message: "Guess a number: ",
@@ -14,3 +18,16 @@ if (userNumber.userGuessed === randomNumber){
     console.log(`Sorry Better Luck Next Time
     The number was: ${randomNumber}`)
 ]);
+let usrAnswer = await inquirer.prompt(
+    {
+        name: "replay",
+        type: "confirm",
+        message: "Do you want to play again??",
+        default: "true",
+    }
+);
+
+condition = usrAnswer.replay;
+};
+
+
